8734af65df030eeb62766be422abea3c87232a8d
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-14 09:53:24 -0300

    Done

---
+++ script.js
@@ -0,0 +1,39 @@
+// ##### Exercício 1 #####
+
+// for(let number = 1; number <= 10; number++){
+//     console.log("Número:", number)
+// }
+
+
+
+// ##### Exercício 2 ##### 
+
+//  let number = 10;
+//  while (number >= 1){
+//      console.log("Número:", number);
+//      number--;
+//     }
+
+
+
+// ##### Exercício 3 #####
+
+// for(let number = 1; number <= 100; number+=2){
+//     console.log("Número:", number)
+// }
+
+
+
+// ##### Exercício 4 #####
+
+// for(let number = 0; number <= 100; number++){
+//     if(number %2 === 0 ){
+//         console.log("Número:", number);
+//     }
+// }
+
+//OU
+
+// for(let number = 0; number <= 100; number+=2){
+//     console.log("Número:", number)
+// }
